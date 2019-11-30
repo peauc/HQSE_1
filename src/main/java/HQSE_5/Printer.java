1adc827058615dfd7f51ed81bd95ec4cb40dc0e5
Blanchard <alexandre.blanchard@epitech.eu>
2019-11-30 15:15:53 +0100

    Link between Chat class and ScannerStandardInput + Printer class

---
+++ src/main/java/HQSE_5/Printer.java
@@ -0,0 +1,12 @@
+package HQSE_5;
+
+public class Printer {
+
+    public void printName(String name) {
+        System.out.println("[" + name + "]");
+    }
+
+    public static void printMessage(String msg) {
+        System.out.println(msg);
+    }
+}
