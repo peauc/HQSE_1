3fcbd98f3fbeb37348a1f9b434a384390153eb7b
Blanchard <alexandre.blanchard@epitech.eu>
2019-11-30 15:22:01 +0100

    Modifying print of prompt without newline

--- src/main/java/HQSE_5/Printer.java
+++ src/main/java/HQSE_5/Printer.java
@@ -3,7 +3,7 @@ package HQSE_5;
 public class Printer {
 
     public void printName(String name) {
-        System.out.println("[" + name + "]");
+        System.out.print("[" + name + "] ");
     }
 
     public static void printMessage(String msg) {
